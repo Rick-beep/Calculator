9e9bd4a6cb3a43e1687b5dc87e76bf5446cb26c3
Chrisg <gilbertchristian25@gmail.com>
2025-12-11 01:47:20 +0900

    perbaiki format entry

--- calkulator.py
+++ calkulator.py
@@ -1,37 +1,45 @@
 import tkinter as tk
 from PIL import Image, ImageTk  # Import Pillow
 
-history = 0
-operasi = ""
-
-def on_button_click(event):
+def on_button_click(event, widget):
     char_pressed = event.char
-    key_symbol = event.keysym
     
-    global operasi
+    current_text = widget.get()
+    new_text = current_text[:]
     
-    if char_pressed:
-        number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",]
-        symbol = ["+", "-", "*","%", "!", "/"]
-        bracket = ["(", ")"]
-        
-        
-        # untuk menghapus index terahir
-        if key_symbol == "BackSpace" and len(operasi) > 0:
-            operasi = operasi[:-1]
-        
-        if char_pressed in number:
-            operasi += char_pressed
-            
-        elif char_pressed in symbol + bracket:
-            if operasi[-1:] in symbol and char_pressed not in bracket:
-                operasi = operasi[:-1]
-            operasi += char_pressed
-            print(char_pressed)
+    number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",]
+    symbol = ["+", "-", "*","%", "!", "/"]
+    bracket = ["(", ")"]
+    
+    # hapus index terakhir
+    if event.keysym == "BackSpace":
+        if len(current_text) > 0:
+            new_text = current_text[:-1]
+            widget.delete(0, tk.END)
+            widget.insert(0, new_text)
+        return "break"
+    
+    
+    if not char_pressed or ord(char_pressed) < 32:
+        return "break"
+    
+    if char_pressed in number:
+        new_text += char_pressed
+        #TODO Perbaiki bagian ini;
+        #jika input "2/(+2)" format seharusmya = "2/(2)"
+    elif char_pressed in symbol + bracket:
+        if new_text[-1] in bracket and char_pressed in symbol:
+            pass
+        if len(new_text) > 0 and new_text[-1] in symbol and char_pressed not in bracket:
+            new_text = new_text[:-1]
+        new_text += char_pressed
 
-            
-    print(operasi)
-
+    widget.delete(0, tk.END)
+    widget.insert(0, new_text)
+    
+    widget.icursor(tk.END)
+    return "break"
+        
 window = tk.Tk()
 window.config(background="grey")
 window.title("Calculator")
@@ -42,17 +50,12 @@ tk_image = ImageTk.PhotoImage(original_image)
 window.iconphoto(True, tk_image)
 
 # text display hasil
-my_label = tk.Label(window, 
-                    text=f"{operasi}", 
+my_label = tk.Entry(window, 
                     font=("Arial", 48),
                     bg="#2a79b5"
                     )
+my_label.focus_set()
 my_label.grid(row=0, column=0) 
  
- 
-# Button (0-9, C, =)
-my_button = tk.Button(window, text="0", command=on_button_click)
-my_button.grid(row=2, column=0)
-
-window.bind("<Key>", on_button_click)
+my_label.bind("<Key>", lambda e: on_button_click(e, my_label))
 window.mainloop()
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,3 @@
-k
+        # untuk menghapus index terahir
+    if key_symbol == "BackSpace" and len(new_text) > 0:
+        new_text = new_text[:-1]
